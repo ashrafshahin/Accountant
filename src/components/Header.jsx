bfc0f3b3055df33d1cdb95d9324e27fa62e721ee
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-05-01 17:09:58 +0100

    only career option added in menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const NAV_LINKS = [
     { label: "Home", href: "/" },
     { label: "About", href: "/about" },
     { label: "Services", href: "/services" },
+    { label: "Careers", href: "/careers" },
     { label: "Contacts", href: "/contacts" },
 ];
 
